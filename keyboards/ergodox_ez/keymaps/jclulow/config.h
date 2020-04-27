76181d9ce2eca8222905288d484837f16b7aecbb
Joshua M. Clulow <josh@sysmgr.org>
2020-04-26 19:25:16 -0700

    initial version, from web configurator

---
+++ keyboards/ergodox_ez/keymaps/jclulow/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#define FIRMWARE_VERSION u8"ABgRE/606yX"
